2ca261a94d4129e57814084ce5d7404ea0bad3c6
wcf <1533144693@qq.com>
2019-05-08 18:24:21 +0800

    update

--- pytext.py
+++ pytext.py
@@ -1,6 +1,10 @@
-print('helloworld')
-for a in range(1,2):
-    print(a)
-
-a=[1,2,3,4,5]
-print(a)
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 1000)
+y=0
+for k in range(1, 10, 1):
+    y=y+4*np.sin((2*k-1)*x)/((2*k-1)*np.pi)
+plt.plot(x,y,'k',color='r',label="w=1",linewidth=3)
+plt.axis([0,10,-1.5,1.5])
+plt.legend()
+plt.show()
